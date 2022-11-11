281dfad5c84b3ce6b78c5842da4f3692e4103549
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-11 09:05:23 +0700

    #6 - Modify TestController :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/TestController.java
+++ src/main/java/com/hendisantika/TestController.java
@@ -1,7 +1,10 @@
 package com.hendisantika;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
  * To change this template use File | Settings | File Templates.
  */
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/")
     public String test() {
-        return "success";
+        log.info("Spring Boot Keycloak Resource Server Sample " + LocalDateTime.now());
+        return "Spring Boot Keycloak Resource Server Sample  " + LocalDateTime.now();
     }
 }
