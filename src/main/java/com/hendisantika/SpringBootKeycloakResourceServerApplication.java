e05418c7d17c5894fe3a62a6f13555e22735ee3d
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-11 08:40:14 +0700

    #4 - Add Servlet Initializer :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/SpringBootKeycloakResourceServerApplication.java
+++ src/main/java/com/hendisantika/SpringBootKeycloakResourceServerApplication.java
@@ -2,12 +2,18 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootKeycloakResourceServerApplication {
+public class SpringBootKeycloakResourceServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootKeycloakResourceServerApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootKeycloakResourceServerApplication.class);
+    }
 }
